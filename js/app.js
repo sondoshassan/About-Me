c4e55dd4015c56ca96a109d0c4ccadb21edbd7c9
sondoshassan <sondos199628@gmail.com>
2020-02-18 19:21:36 +0200

    add two questions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 'use strict';
+var score = 0;
 var gistName = prompt ("please, what's your name?");
 alert ("welcome " + gistName + " I hope you to enjoy");
 var name = prompt("my name is sondos","yes or no");
 name = name.toLowerCase();
 console.log (name);
-if (name === "yes" || name === "y"){
+if (name === 'yes' || name === 'y'){
     alert ("yes your good guesser my  name is sondos");
+    score++;
 }
 else if (name == "no" || name == "n"){
     alert ("wrong answer");
 }
+
 var color = prompt ("did you think I like black color?","yes or no");
 color = color.toUpperCase();
 console.log ("this for color question " + color);
 switch (color) {
     case "YES":
-        alert ("Yes I like the black color");
-        break;
     case "Y":
-         alert ("Yes I like the black color");
-            break;
+        alert ("Yes I like the black color");
+        score++;
+        break;
     case "NO":
-        alert ("try again!");
-            break;
     case "N":
-        alert ("try again!");
-          break;
+        alert ("wrong answer!");
+            break;
           default:
         break;
 }
@@ -35,26 +35,25 @@ animle = animle.toLowerCase();
 console.log ("the animle answer "+ animle);
 if (animle === "yes" || animle === "y"){
 alert ("no, I hate the dog");
+
 }
 else if (animle === "no" || animle === "n"){
     alert ("yes you get it");
+    score++;
 }
 var subject = prompt ("guess I like a math","yes or no");
 subject = subject.toLowerCase ();
 console.log("my favorite subject "+ subject);
 switch (subject) {
     case "yes":
+    case "y":
         alert("yes I hope you also like math");
+        score++;
         break;
-        case "y":
-            alert("yes I hope you also like math");
-            break;
         case "no":
+        case "n":
          alert("no math is my favorite subject");
                 break;
-        case "n":
-            alert("no math is my favourite subject");
-             break;
     default:
         break;
 }
@@ -63,8 +62,77 @@ hobby = hobby.toUpperCase();
 console.log ("about my hobby " + hobby);
 if (hobby === "YES" || hobby === "Y"){
     alert ("yes the reading is good to get a knowledge");
+    score++;
 }
 else if (hobby === "NO" || hobby === "N"){
     alert ("no my friend it's my favorite hobby");
 }
 alert (gistName + ", thanks for playing with me");
+
+// question sex
+var myAge = prompt ('how much am I old?','using number [15,30]');
+myAge = Number(myAge);
+var i = 0;
+if (myAge === 23){
+    alert ('yes I\'m in 23');
+    score++;
+}
+while (myAge !== 23 && i<3){
+    
+if (myAge > 23){
+    alert ('is high');     
+      }
+else if (myAge < 23){
+        alert ('is low');     
+          }
+          i++;
+          var myAge = prompt ('how much am I old?','using number');
+          myAge = Number(myAge);
+if (myAge == 23)
+          {
+            alert ('yes I\'m in 23');
+            score++;  
+          }
+    }
+if (myAge !== 23 && i == 3){
+    alert ('I\'m in 23'); 
+}
+console.log('score at q6 '+score);
+
+//  question seven
+var langPrograme = ['c++','javascript','java','html','css'];
+var ansUser = prompt ('what are the programming languages I know?','c++, javascript, java, html, css');
+ansUser = ansUser.toLowerCase();
+
+var j = 0;
+for (var k=0; k < 5;k++){
+    var p = 0;
+for (var l = 0; l < langPrograme.length; l++){
+    
+if (ansUser === langPrograme[l]){
+    alert ('that\'s right');
+    score++;
+    p++;
+    break;   
+}
+}
+console.log ('k = ' + k);
+if (p !== 1){
+    alert ('thats wrong');
+    var ansUser = prompt ('what are the programming languages I know?','c++, javascript, java, html, css');
+    
+}
+else if (ansUser === langPrograme[l]) {
+    break;
+}
+}
+console.log ('k out loop = '+ k);
+if (ansUser !== langPrograme[l] && k === 5){
+    alert ('thats wrong');
+}
+console.log('score at q7 '+score);
+
+
+alert ('my favorite movies are '+langPrograme);
+alert ('your score is '+score+' from 7');
+
